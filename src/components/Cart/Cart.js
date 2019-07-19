fd241d29d897e20b18428f073c02519a90f1ce83
kimutai <georgekimuu@gmail.com>
2019-07-19 11:46:42 +0300

    second_commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CartItem from './CartItem';
+import { Button} from 'react-bootstrap';
+import { Modal} from 'react-bootstrap';
+import { useState,render } from 'react';
+
+
+
 
 const Cart = ({ items, total, currency, removeFromCart }) => {
+    
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div>
             <h3>My Cart</h3>
@@ -21,9 +32,39 @@ const Cart = ({ items, total, currency, removeFromCart }) => {
                             <div className="alert alert-info">No Items Selected</div>
                         )}
                         <div className="cart__total">Total: {total} {currency}</div>
+                        <>
+        <Button variant="primary" onClick={handleShow}>
+          Check Out
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body> {items.map(item => (
+                                    <CartItem key={item.id} {...item} onClick={() => removeFromCart(item.id)} />
+                                ))}
+                            
+                        )}
+                        {items.length === 0 && (
+                            <div className="alert alert-info">No Items Selected</div>
+                        )}
+                        </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+                            
                     </div>
                 </div>
-            </div>
+                
+          </div>
         </div>
     );
 }
